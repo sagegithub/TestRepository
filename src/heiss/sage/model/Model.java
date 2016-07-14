f68fd2836e141580e4ee62d5474a8cd4d30cd006
Sage Heiss <Sage_Heiss@hermanmiller.com>
2016-07-14 15:31:08 -0400

    First commit in Sage Test Project 01.

---
+++ src/heiss/sage/model/Model.java
@@ -0,0 +1,9 @@
+package heiss.sage.model;
+
+public class Model {
+
+	public Model() {
+		System.out.println("Create new Model in Sage Test Project 01");
+	}
+	
+}
---
+++ src/heiss/sage/view/View.java
@@ -0,0 +1,9 @@
+package heiss.sage.view;
+
+public class View {
+
+	public View() {
+		System.out.println("Create new View in Sage Test Project 01");
+	}
+
+}
