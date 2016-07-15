c64f585d3b161b0764c7a63cf62f9354afd3abb8
Sage Heiss <Sage_Heiss@hermanmiller.com>
2016-07-15 13:35:14 -0400

    Made change which should conflict with branch01

--- src/heiss/sage/view/View.java
+++ src/heiss/sage/view/View.java
@@ -3,7 +3,7 @@ package heiss.sage.view;
 public class View {
 
 	public View() {
-		System.out.println("Create new View in Sage Test Project 01");
+		System.out.println("Create new View in Sage Test Project 01 - Master");
 	}
 
 }
